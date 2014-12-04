569ee7c0a1e9ae5bc61825380de5f693964997e0
pachu <pachuc@vt.edu>
2014-12-03 22:38:03 -0500

    class done, need to write lqr control function

--- QuadRotor.m
+++ QuadRotor.m
@@ -67,7 +67,6 @@ classdef QuadRotor < handle
         obj.psidot = [];
         obj.cur_state = [0 0 0 0 0 0 0 0 0 0 0 0];
         obj.start_state = [0 0 0 0 0 0 0 0 0 0 0 0];
-        obj.end_state = [0 0 0 0 0 0 0 0 0 0 0 0];
         end
         
         
@@ -114,25 +113,175 @@ classdef QuadRotor < handle
             obj.psidot(obj.step_count) = obj.cur_state(12);
         end
         
+        function controlvec = getControl(obj)
+        end
         function nextState(obj)
-            
+            controlvec = obj.getControl();
+            obj.cur_state = quadr(controlvec, obj.cur_state, obj.time_step);
             obj.step_count = obj.step_count+1;
             
         end
         
         function startSim(obj)
+            obj.cur_state = obj.start_state;
             time = obj.time_step * obj.step_count;
             
-            while time < obj.run_time
+            while time <= obj.run_time
                 obj.recordState();
                 obj.nextState();
                 time = obj.time_step * obj.step_count;
             end
+            
+            obj.stopSim();
         end
         
+        function stopSim(obj)
+            disp(obj.x);
+            disp(obj.y);
+            disp(obj.z);
+            disp(obj.phi);
+            disp(obj.theta);
+            disp(obj.psi);
+            disp(obj.xdot);
+            disp(obj.ydot);
+            disp(obj.zdot);
+            disp(obj.phidot);
+            disp(obj.thetadot);
+            disp(obj.psidot);
+            
+        end
         
         
     end
     
 end
 
+function nextState = quadr(controlvec, statevec, timestep)
+
+x = statevec(1);
+y = statevec(2);
+z = statevec(3);
+phi = statevec(4);
+theta = statevec(5);
+psi = statevec(6);
+xdot = statevec(7);
+ydot = statevec(8);
+zdot = statevec(9);
+phidot = statevec(10);
+thetadot = statevec(11);
+psidot = statevec(12);
+U1 = controlvec(1);
+U2 = controlvec(2);
+U3 = controlvec(3);
+U4 = controlvec(4);
+
+[U1_2, U2_2, U3_2, U4_2, omega] = Omegasquared(U1, U2, U3, U4);
+
+[phidotdot, thetadotdot, psidotdot] = Angles(phidot, thetadot, psidot, U2_2, omega, U4_2, U3_2);
+
+phidot = phidot + (phidotdot * timestep);
+thetadot = thetadot + (thetadotdot * timestep);
+psidot = psidot + (psidotdot * timestep);
+
+phi = phi + (phidot * timestep);
+theta = theta + (thetadot * timestep);
+psi = psi + (psidot * timestep);
+
+[xdotdot, ydotdot, zdotdot] = Displacement(phi, theta, psi, U1_2);
+xdot = xdot + (xdotdot * timestep);
+ydot = ydot + (ydotdot * timestep);
+zdot = zdot + (zdotdot * timestep);
+
+x = x + (xdot * timestep);
+y = y + (ydot * timestep);
+z = z + (zdot * timestep);
+
+nextState = [x y z phi theta psi xdot ydot zdot phidot thetadot psidot];
+
+end
+
+function [U1_2, U2_2, U3_2, U4_2, omega] = Omegasquared(U1, U2, U3, U4)
+b = 1; %thrust factor  (kg.m) 
+d = 0.3048;  % Drag factor
+l = 0.23;  % Distance to the center of the Quadrotor
+
+T1 = U1/(4*b);
+T2 = U2/(2*b*l);
+T3 = U3/(2*b*l);
+T4 = U4/(4*d);
+
+OS1 = T1 + T3 - T4;
+OS2 = T1 - T2 + T4;
+OS3 = T1 - T3 - T4;
+OS4 = T1 + T2 + T4;
+OS5 = OS2 - OS1 - OS3 + OS4;
+
+omega = dot(OS5, d);
+
+OS1cal = OS1 + OS2 + OS3 + OS4;
+OS2cal = OS4 - OS2;
+OS3cal = OS1 - OS3;
+OS4cal = OS2 - OS1 - OS3 + OS4;
+
+U1_2 = b * OS1cal;
+U2_2 = b * OS2cal;
+U3_2 = b * OS3cal;
+U4_2 = b * OS4cal;
+
+end
+
+function [xdotdot ydotdot zdotdot] = Displacement(phi, theta, psi, U1)
+m = 1.89;  % Mass of the Quadrotor in Kg
+g = 9.81;   % Gravitational acceleration
+
+
+POE = cos(phi) * sin(theta) * cos(psi);
+POE1 = sin(phi) * sin(psi);
+POE2 = cos(phi) * sin(theta) *sin(psi);
+POE3 = sin(phi) * cos(psi);
+POE4 = cos(phi) * cos(theta);
+
+D1 = U1/m;
+
+xdotdot = (POE + POE1) * D1;
+ydotdot = (POE2 + POE3) * D1;
+zdotdot = g - (POE4 *D1);
+
+end
+
+function [phidotdot thetadotdot psidotdot] = Angles(phidot, thetadot, psidot, U2, omega, U4, U3)
+
+%moment of intertia units: kg.m^2
+Ix = 2.297e-2;  % Quadrotor moment of inertia around X axis
+Iy = 2.297e-2;  % Quadrotor moment of inertia around Y axis
+Iz = 4.935e-2;  % Quadrotor moment of inertia around Z axis
+%%%%%%%%
+%need to check this param
+Jr = 6.5*10^(-5);  % Total rotational moment of inertia around the propeller axis
+%k  = 6e-5;					% rotor inertia (kg.m^2) same param?
+%k = Jr; %set to 2?
+
+a1 = (Iy - Iz)/Ix;
+a2 = Jr/Ix;
+b1 = l/Ix;
+a3 = (Iz-Ix)/Iy;
+a4 = Jr/Iy;
+b2 = l/Iy;
+a5 = (Ix-Iy)/Iz;
+b3 = l/Iz;
+
+POE = a1 * thetadot * psidot;
+POE1 = a2 * thetadot * omega;
+POE2 = b1 * U2;
+phidotdot = POE + POE1 + POE2;
+
+POE3 = a3 * phidot * psidot;
+POE4 = a4 * phidot * omega;
+POE5 = b2 * U3;
+thetadotdot = POE3 + POE4 + POE5;
+
+POE6 = a5 * phidot * thetadot;
+POE7 = b3 * U4;
+psidotdot = POE6 + POE7;
+
+end
--- myLQR.m
+++ myLQR.m
@@ -8,7 +8,7 @@ clc %clear the command window
 clear all %clear all variables
 close all %close all scripts.
 
-global Jr Ix Iy Iz b d l m g; %Kpz Kdz Kpp Kdp Kpt Kdt Kpps Kdps ZdF PhidF ThetadF PsidF ztime phitime thetatime psitime Zinit Phiinit Thetainit Psiinit Uone Utwo Uthree Ufour Ez Ep Et Eps
+global Jr Ix Iy Iz b d l m g;
 
 % Quadrotor constants
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -32,22 +32,7 @@ m = 1.89;  % Mass of the Quadrotor in Kg
 g = 9.81;   % Gravitational acceleration
 
 %quadrotor state vars:
-global x y z phi theta psi xdot ydot zdot phidot thetadot psidot;
-global timestep;
-
-%inint the state vars:
-x = 0;
-y = 0;
-z = 0;
-phi = 0;
-theta = 0;
-psi = 0;
-xdot = 0;
-ydot = 0;
-zdot = 0;
-phidot = 0;
-thetadot = 0;
-psidot = 0;    
+global timestep;    
 timestep = 0.02;%seconds    
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -74,10 +59,6 @@ initR = [0 0 -2];
 initP = [0 0 0];
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-%xd = zeros(12, 1);
-xcur = [0 0 5 0 0 0 0 0 0 0 0 0];
-xd = [0 0 0 0 0 0 0 0 0 0 0 0];	% desired state
 delta = 0; %pi/4;					% yaw at linearization point
 
 % LQR Controller
@@ -104,51 +85,71 @@ save K.mat K
 
 %the sim running my functions:
 
+state = [0 0 0 0 0 0 0 0 0 0 0 0];%current state
+xd = [0 0 1 0 0 0 0 0 0 0 0 0];	% desired state
 
-for time = 1:100
+%simulation total run time.
+simlength = 20;
+
+%vars for graphing.
+x = zeros(1, simlength);
+y = zeros(1, simlength);
+z = zeros(1, simlength);
+phi = zeros(1, simlength);
+theta = zeros(1, simlength);
+psi = zeros(1, simlength);
+xdot = zeros(1, simlength);
+ydot = zeros(1, simlength);
+zdot = zeros(1, simlength);
+phidot = zeros(1, simlength);
+thetadot = zeros(1, simlength);
+psidot = zeros(1, simlength);
+
+for time = 1:simlength
+    
+    %store the state vars for graphing.
+    x(time) = state(1);
+    y(time) = state(2);
+    z(time) = state(3);
+    phi(time) = state(4);
+    theta(time) = state(5);
+    psi(time) = state(6);
+    xdot(time) = state(7);
+    ydot(time) = state(8);
+    zdot(time) = state(9);
+    phidot(time) = state(10);
+    thetadot(time) = state(11);
+    psidot(time) = state(12);
     
     fprintf('Time: %d\n', (time*timestep));
-    fprintf('x: %d\n', x);
-    fprintf('y: %d\n', y);
-    fprintf('z: %d\n', z);
-    fprintf('phi: %d\n', phi);
-    fprintf('theta: %d\n', theta);
-    fprintf('psi: %d\n', psi);
-    fprintf('xdot: %d\n', xdot);
-    fprintf('ydot: %d\n', ydot);
-    fprintf('zdot: %d\n', zdot);
-    fprintf('phidot: %d\n', phidot);
-    fprintf('thetadot: %d\n', thetadot);
-    fprintf('psidot: %d\n', psidot);
+    fprintf('x: %d\n', state(1));
+    fprintf('y: %d\n', state(2));
+    fprintf('z: %d\n', state(3));
+    fprintf('phi: %d\n', state(4));
+    fprintf('theta: %d\n', state(5));
+    fprintf('psi: %d\n', state(6));
+    fprintf('xdot: %d\n', state(7));
+    fprintf('ydot: %d\n', state(8));
+    fprintf('zdot: %d\n', state(9));
+    fprintf('phidot: %d\n', state(10));
+    fprintf('thetadot: %d\n', state(11));
+    fprintf('psidot: %d\n', state(12));
     
     %difference vector.
     xdiff = [0 0 0 0 0 0 0 0 0 0 0 0];
     
-%     xdiff(1) = x - xd(1);
-%     xdiff(2) = y - xd(2);
-%     xdiff(3) = z - xd(3);
-%     xdiff(4) = phi - xd(4);
-%     xdiff(5) = theta - xd(5);
-%     xdiff(6) = psi - xd(6);
-%     xdiff(7) = xdot - xd(7);
-%     xdiff(8) = ydot - xd(8);
-%     xdiff(9) = zdot - xd(9);
-%     xdiff(10) = phidot - xd(10);
-%     xdiff(11) = thetadot - xd(11);
-%     xdiff(12) = psidot - xd(12);
-
-    xdiff(1) = xd(1) - x;
-    xdiff(2) = xd(2) - y;
-    xdiff(3) = xd(3) - z;
-    xdiff(4) = xd(4) - phi;
-    xdiff(5) = xd(5) - theta;
-    xdiff(6) = xd(6) - psi;
-    xdiff(7) = xd(7) - xdot;
-    xdiff(8) = xd(8) - ydot;
-    xdiff(9) = xd(9) - zdot;
-    xdiff(10) = xd(10) - phidot;
-    xdiff(11) = xd(11) - thetadot;
-    xdiff(12) = xd(12) - psidot;
+    xdiff(1) = state(1) - xd(1);
+    xdiff(2) = state(2) - xd(2);
+    xdiff(3) = state(3) - xd(3);
+    xdiff(4) = state(4) - xd(4);
+    xdiff(5) = state(5) - xd(5);
+    xdiff(6) = state(6) - xd(6);
+    xdiff(7) = state(7) - xd(7);
+    xdiff(8) = state(8) - xd(8);
+    xdiff(9) = state(9) - xd(9);
+    xdiff(10) = state(10) - xd(10);
+    xdiff(11) = state(11) - xd(11);
+    xdiff(12) = state(12) - xd(12);
     
     xt = transpose(xdiff);
     u = K * xt;
@@ -157,7 +158,7 @@ for time = 1:100
     U2 = u(2);
     U3 = u(3);
     U4 = u(4);
-    quadr(U1, U2, U3, U4);
+    state = quadr(U1, U2, U3, U4, state);
     
     fprintf('U1: %d\n', U1);
     fprintf('U2: %d\n', U2);
@@ -166,115 +167,6 @@ for time = 1:100
     fprintf('----------------------------------------\n');   
     
 end
-% % Controlling the Quadrotor
-%sim('QuadSimulink');
-
-%disp(xcur)
-%disp(xd);
-%disp(K);
-%xt = transpose(xcur);
-%disp(xt);
-%u = K * transpose(xcur);
-%disp(u);
-
-end
-
-function [xdotdot ydotdot zdotdot] = Displacement(phi, theta, psi, U1)
-global m g;
-
-POE = cos(phi) * sin(theta) * cos(psi);
-POE1 = sin(phi) * sin(psi);
-POE2 = cos(phi) * sin(theta) *sin(psi);
-POE3 = sin(phi) * cos(psi);
-POE4 = cos(phi) * cos(theta);
-
-D1 = U1/m;
-xdotdot = (POE + POE1) * D1;
-ydotdot = (POE2 + POE3) * D1;
-zdotdot = g - (POE4 *D1);
-
-end
-
-function [phidotdot thetadotdot psidotdot] = Angles(phidot, thetadot, psidot, U2, omega, U4, U3)
-global Jr Ix Iy Iz b d l m g;
-
-a1 = (Iy - Iz)/Ix;
-a2 = Jr/Ix;
-b1 = l/Ix;
-a3 = (Iz-Ix)/Iy;
-a4 = Jr/Iy;
-b2 = l/Iy;
-a5 = (Ix-Iy)/Iz;
-b3 = l/Iz;
-
-POE = a1 * thetadot * psidot;
-POE1 = a2 * thetadot * omega;
-POE2 = b1 * U2;
-phidotdot = POE + POE1 + POE2;
-
-POE3 = a3 * phidot * psidot;
-POE4 = a4 * phidot * omega;
-POE5 = b2 * U3;
-thetadotdot = POE3 + POE4 + POE5;
-
-POE6 = a5 * phidot * thetadot;
-POE7 = b3 * U4;
-psidotdot = POE6 + POE7;
-
-end
-
-function [U1_2, U2_2, U3_2, U4_2, omega] = omegasquared(U1, U2, U3, U4)
-global b l d;
-
-T1 = U1/(4*b);
-T2 = U2/(2*b*l);
-T3 = U3/(2*b*l);
-T4 = U4/(4*d);
-
-OS1 = T1 + T3 - T4;
-OS2 = T1 - T2 + T4;
-OS3 = T1 - T3 - T4;
-OS4 = T1 + T2 + T4;
-OS5 = OS2 - OS1 - OS3 + OS4;
-
-omega = dot(OS5, d);
-
-OS1cal = OS1 + OS2 + OS3 + OS4;
-OS2cal = OS4 - OS2;
-OS3cal = OS1 - OS3;
-OS4cal = OS2 - OS1 - OS3 + OS4;
-
-U1_2 = b * OS1cal;
-U2_2 = b * OS2cal;
-U3_2 = b * OS3cal;
-U4_2 = b * OS4cal;
-
-end
-
-function quadr(U1, U2, U3, U4)
-
-global x y z phi theta psi xdot ydot zdot phidot thetadot psidot;
-global timestep;
-
-[U1_2, U2_2, U3_2, U4_2, omega] = omegasquared(U1, U2, U3, U4);
-
-[phidotdot, thetadotdot, psidotdot] = Angles(phidot, thetadot, psidot, U2_2, omega, U4_2, U3_2);
-
-phidot = phidot + (phidotdot * timestep);
-thetadot = thetadot + (thetadotdot * timestep);
-psidot = psidot + (psidotdot * timestep);
-
-phi = phi + (phidot * timestep);
-theta = theta + (thetadot * timestep);
-psi = psi + (psidot * timestep);
-
-[xdotdot, ydotdot, zdotdot] = Displacement(phi, theta, psi, U1_2);
-xdot = xdot + (xdotdot * timestep);
-ydot = ydot + (ydotdot * timestep);
-zdot = zdot + (zdotdot * timestep);
-
-x = x + (xdot * timestep);
-y = y + (ydot * timestep);
-z = z + (zdot * timestep);
-
+format shortG;
+disp(x);
 end
